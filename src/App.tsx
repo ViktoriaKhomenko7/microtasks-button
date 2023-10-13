42b9743bdfead345e0df69553a36a58fb4f410d1
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-10-13 19:50:06 +0300

    training

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hello, I'm Vasya")
+    // }
+    //
+    // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log("Hello, I'm Ivan")
+    // }
+
+    const onClickHandler = (name: string) => {
+        console.log(name)
+    }
+
+    const foo1 = () => {
+// Ничего не передавать, но в консоли выведет 100200
+        console.log(100200)
+    }
+
+    const foo2 = (num: number) => {
+// передать цифру 100200
+        console.log(num)
+    }
+
+
+    return (
+        <div className="App">
+            {/*<button onClick={(event)=>{console.log("Hello")}}>MyYouTubeChanel-1</button>*/}
+
+
+            <button onClick={(event)=>onClickHandler("Vasya")}>MyYouTubeChanel-1</button>
+            <button onClick={(event)=>onClickHandler("Ivan")}>MyYouTubeChanel-2</button>
+
+
+            {/*<button onClick={()=>onClickHandler("some info")}>MyYouTubeChanel-3</button>*/}
+
+
+
+            <button onClick={foo1}>1</button>
+            <button onClick={()=>foo2(100200)}>2</button>
+        </div>
+    );
 }
 
 export default App;
